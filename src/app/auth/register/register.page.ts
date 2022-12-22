487b071f138755a7235133d0aff97cbc9f0cff9d
joherrerac <jo.herrerac@duocuc.cl>
2022-12-22 18:24:14 -0300

    integracion con firebase terminada

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../services/firestore.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(private firestore: FirestoreService) { }
 
   ngOnInit() {
   }
 
+  getPrueba()
+{
+  this.firestore.getCollection()
+}
 }
